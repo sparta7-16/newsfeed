73fb851c4ed21d8cb065b8bc53dfefab28ca2b18
dbtmdgml98 <dbtmdgml98@naver.com>
2024-11-20 15:48:48 +0900

    Feat: 로그인필터 v2

--- src/main/java/com/example/newsfeed/controller/UserController.java
+++ src/main/java/com/example/newsfeed/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.example.newsfeed.controller;
 
+import com.example.newsfeed.dto.user.LoginRequestDto;
 import com.example.newsfeed.dto.user.SignupUserRequestDto;
+import com.example.newsfeed.entity.User;
 import com.example.newsfeed.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
@@ -23,9 +28,25 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody LoginRequestDto loginRequestDto) {
+    public ResponseEntity<String> loginUser(@RequestBody LoginRequestDto loginRequestDto, HttpServletRequest request) {
         User loginedUser = userService.loginUser(loginRequestDto);
 
-        return new ResponseEntity<>(HttpStatus.OK), "로그인 되었습니다.";
+        // 로그인 성공했으니까 Session 등록
+        HttpSession session = request.getSession();
+        session.setAttribute("SESSION_KEY", loginedUser.getUserId());
+
+        return new ResponseEntity<>("로그인 되었습니다.", HttpStatus.OK);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logoutUser(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        // 세션 존재하면 무효화 처리
+        if (session != null) {
+            session.invalidate();
+        }
+
+        return new ResponseEntity<>("로그아웃 되었습니다.", HttpStatus.OK);
     }
 }
--- src/main/java/com/example/newsfeed/filter/LoginFilter.java
+++ src/main/java/com/example/newsfeed/filter/LoginFilter.java
@@ -5,15 +5,45 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
 
 import java.io.IOException;
 
+@Slf4j
 public class LoginFilter implements Filter {
 
-    public static final String[]
+    // 회원가입, 로그인 요청은 인증 처리에서 제외
+    private static final String[] WHITE_LIST = {"/users", "/users/login"};
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 다운캐스팅
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
 
+        log.info("로그인 필터 로직 실행");
+
+        // WHITE_LIST에 포함되어 있지 않다면
+        if (!isWhiteList(requestURI)) {
+
+            // 로그인 확인
+            HttpSession session = httpRequest.getSession(false);
+            // 로그인하지 않은 사용자일 경우
+            if (session == null || session.getAttribute("SESSION_KEY") == null) {
+                throw new RuntimeException("로그인 해주세요.");
+            }
+
+            // 로그인 한 사용자일 경우
+            log.info("로그인에 성공했습니다. 로그인된 사용자 요청: {}", requestURI);
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private boolean isWhiteList(String requestURI) {
+        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
     }
 }
