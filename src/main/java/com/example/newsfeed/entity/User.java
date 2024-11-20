e869e18a7d591ff280269c68b258fb4a4775cc5b
2unmi <2unmin4030@gmail.com>
2024-11-20 12:19:40 +0900

    add : 초기 프로젝트 셋팅 추가

--- src/main/java/com/example/newsfeed/entity/User.java
+++ src/main/java/com/example/newsfeed/entity/User.java
@@ -1,4 +1,18 @@
 package com.example.newsfeed.entity;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Long userId;
+    @Column(name = "user_name")
+    private String username; // 유저 이름
+    private String email; //이메일
+    private String password; //패스워드
 }
