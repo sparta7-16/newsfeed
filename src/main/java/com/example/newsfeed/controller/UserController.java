4efbe1af4084f3a7a0e8d4e7b3378215564f6b2b
2unmi <2unmin4030@gmail.com>
2024-11-20 15:43:34 +0900

    feat : 사용자 정보 조회 기능구현
    전체 사용자를 조회하거나 특정 사용자를 조회 할 수 있는 기능을 구현

--- src/main/java/com/example/newsfeed/controller/UserController.java
+++ src/main/java/com/example/newsfeed/controller/UserController.java
@@ -1,15 +1,16 @@
 package com.example.newsfeed.controller;
 
+import com.example.newsfeed.dto.user.ReadUserResponseDto;
 import com.example.newsfeed.dto.user.SignupUserRequestDto;
 import com.example.newsfeed.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
@@ -19,6 +20,16 @@ public class UserController {
     public ResponseEntity<String> signup(@Validated @RequestBody SignupUserRequestDto signupUserRequestDto) {
         String msg = userService.signup(signupUserRequestDto);
         return new ResponseEntity<>(msg,HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<ReadUserResponseDto>> findAll(){
+        List<ReadUserResponseDto> responseDtos = userService.findAllUser();
+        return new ResponseEntity<>(responseDtos,HttpStatus.OK);
 
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ReadUserResponseDto> findUserById(@PathVariable Long id) {
+        ReadUserResponseDto userById = userService.findUserById(id);
+        return new ResponseEntity<>(userById,HttpStatus.OK);
     }
 }
