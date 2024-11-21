d4e33681914cb50aba3076554630861fb3157f61
kangjunhyuk <kangjun1290@gmail.com>
2024-11-21 17:06:14 +0900

    fix : User기능 초기화
    잘못된 pull로 인한 User기능 삭제

--- src/main/java/com/example/newsfeed/respository/UserRepository.java
+++ src/main/java/com/example/newsfeed/respository/UserRepository.java
@@ -1,9 +1,4 @@
 package com.example.newsfeed.respository;
 
-import com.example.newsfeed.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public class UserRepository {
 }
--- src/main/java/com/example/newsfeed/service/UserService.java
+++ src/main/java/com/example/newsfeed/service/UserService.java
@@ -1,18 +1,4 @@
 package com.example.newsfeed.service;
 
-import com.example.newsfeed.dto.user.SignupUserRequestDto;
-import com.example.newsfeed.entity.User;
-import com.example.newsfeed.respository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-@RequiredArgsConstructor
-@Service
 public class UserService {
-    private final UserRepository userRepository;
-
-    public String signup(SignupUserRequestDto signupUserRequestDto) {
-        User user = new User(signupUserRequestDto);
-        User savedUser = userRepository.save(user);
-        return "가입완료하였습니다";
-    }
 }
