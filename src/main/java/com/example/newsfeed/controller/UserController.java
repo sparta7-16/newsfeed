5e08c5b00ed518858bf7c573b776ab7d0d6f06a6
dbtmdgml98 <dbtmdgml98@naver.com>
2024-11-21 19:30:57 +0900

    Fix: 로그인 응답 메세지 수정

--- src/main/java/com/example/newsfeed/controller/UserController.java
+++ src/main/java/com/example/newsfeed/controller/UserController.java
@@ -58,9 +58,8 @@ public class UserController {
         session.setAttribute("SESSION_KEY", loginedUser.getUserId());
 
 //        return new ResponseEntity<>("로그인 되었습니다.", HttpStatus.OK);
-        return ResponseEntity.ok().body("정상적으로 로그인되었습니다.");
+        return ResponseEntity.ok().body("로그인되었습니다");
     }
-
 
     @DeleteMapping("/logout")
     public ResponseEntity<String> logoutUser(HttpServletRequest request) {
