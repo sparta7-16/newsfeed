48b70b701349f46c690256941ddbfca4c52b667a
2unmi <2unmin4030@gmail.com>
2024-11-22 23:59:32 +0900

    add: user와 관련된 연관관계 설정 추가

--- src/main/java/com/example/newsfeed/entity/User.java
+++ src/main/java/com/example/newsfeed/entity/User.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -21,15 +23,24 @@ public class User extends BaseEntity {
     private String email; //이메일
     private String password; //패스워드
     @Setter
-    private LocalDateTime leave_date; // 탈퇴날짜
+    @Column(name = "leave_date")
+    private LocalDateTime leaveDate; // 탈퇴날짜
     @Column(name = "user_status")
     @Setter
     private String userStatus="Y"; // 회원상태
+    @OneToMany(mappedBy = "user",cascade = CascadeType.REMOVE)
+    private List<Post>posts=new ArrayList<>();
+    @OneToMany(mappedBy = "toUser")
+    private List<Friend>sendFriends=new ArrayList<>();
+    @OneToMany(mappedBy = "fromUser",cascade = CascadeType.REMOVE)
+    private List<Friend>receiveFriends=new ArrayList<>();
     public User(SignupUserRequestDto signupUserRequestDto, String password) {
         this.username=signupUserRequestDto.getUsername();
         this.email=signupUserRequestDto.getEmail();
         this.password=password;
     }
+
+
 
     public void updateUser(String username) {
 
