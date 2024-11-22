d5fbecfba67eb49832e69f4c3f4ffa66c988e7e1
Jun Heo <huhjune98@naver.com>
2024-11-22 02:25:01 -0500

    feat: 친구 요청 수락,거절 추가

--- src/main/java/com/example/newsfeed/service/FriendService.java
+++ src/main/java/com/example/newsfeed/service/FriendService.java
@@ -11,6 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -21,6 +24,11 @@ public class FriendService {
 
     //친구 요청 보내기
     public FriendResponseDto sendFriendRequest(Long toUserId, Long fromUserId) throws BadRequestException {
+        // 자기 자신에게 친구 요청 금지
+        if (toUserId.equals(fromUserId)) {
+            throw new BadRequestException("자기 자신에게 친구 요청을 보낼 수 없습니다.");
+        }
+
         // 중복 요청 채크
         if(friendRepository.existsByToUser_UserIdAndFromUser_UserId(toUserId, fromUserId)) {
             throw new BadRequestException("이미 친구 요청을 보냈습니다");
@@ -38,6 +46,40 @@ public class FriendService {
         friendRepository.save(friend);
 
         return new FriendResponseDto(" 요청 완료 되었습니다.");
+    }
+
+    //친구 요청 목록 확인
+    public List<FriendResponseDto> getFriendsRequests(Long userId) {
+        //로그인 사용자가 받은 친구 요청 목록 조회
+        List<Friend> requests = friendRepository.findByToUser_UserIdAndAreWeFriend(userId, false);
+
+        return requests.stream()
+                .map(request -> new FriendResponseDto(
+                        "요청 보낸 사용자: " + request.getFromUser().getUsername()))
+                .collect(Collectors.toList());
+    }
+
+    //친구 요청 수락
+    public FriendResponseDto acceptFriendRequest(Long friendId) throws BadRequestException {
+        //요청 확인
+        Friend friend = friendRepository.findById(friendId)
+                .orElseThrow(()-> new BadRequestException("친구 요청을 찾을 수 없습니다"));
+
+        friend.setAreWeFriend(true);
+        friendRepository.save(friend);
+
+        return new FriendResponseDto(friend.getFromUser().getUsername() + "와 친구가 되었습니다");
+    }
+
+    //친구 요청 거절
+    public FriendResponseDto declineFriendRequest(Long friendId) throws BadRequestException {
+        //요청 확인
+        Friend friend = friendRepository.findById(friendId)
+                .orElseThrow(()-> new BadRequestException("친구 요청을 찾을 수 없습니다"));
+
+        //요청 삭지
+        friendRepository.delete(friend);
+        return new FriendResponseDto(friend.getFromUser().getUsername() + "의 요청을 거절했습니다.");
     }
 
     //친구 삭제
