d8ecdca2b2ade93fd21597f117f9c47d039e0c23
kangjunhyuk <kangjun1290@gmail.com>
2024-11-22 11:17:16 +0900

    feat : 코드 복사
    기존의 post기능구현 브랜치에서 user기능을 잘못 pull해 main기반으로 다시팜

---
+++ src/main/java/com/example/newsfeed/dto/post/PostResponseDto.java
@@ -0,0 +1,36 @@
+package com.example.newsfeed.dto.post;
+
+import com.example.newsfeed.entity.Post;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Getter
+public class PostResponseDto {
+
+    private final Long postId;
+    private final String title;
+    private final String content;
+    private final LocalDate createdDate;
+    private final LocalDate updateDate;
+
+    public PostResponseDto(Long PostId, String title, String content, LocalDate createdDate, LocalDate updateDate) {
+        this.postId = PostId;
+        this.title = title;
+        this.content = content;
+        this.createdDate = createdDate;
+        this.updateDate = updateDate;
+    }
+
+    public static PostResponseDto toDto(Post post) {
+        return new PostResponseDto(
+                post.getPostId(),
+                post.getTitle(),
+                post.getContent(),
+                post.getCreatedDate(),
+                post.getUpdatedDate()
+        );
+    }
+}
